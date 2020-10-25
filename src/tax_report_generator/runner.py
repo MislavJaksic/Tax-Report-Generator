f17931ee6e0967282bfe43bad81fb3dbd53a79b3
Mislav Jaksic <jaksicmislav@gmail.com>
2020-10-25 21:56:10 +0100

    Switched to Poetry

--- src/tax_report_generator/runner.py
+++ src/tax_report_generator/runner.py
@@ -15,29 +15,28 @@ from tax_report_generator.xml_generating import zp_generator
 from tax_report_generator.data_wrangling import pandas_functions
 
 
-
-#Main will be run if you run this script directly
+# Main will be run if you run this script directly
 def Main():
-  command_line_parser = parser.Parser()
-  arguments = command_line_parser.GetArguments()
-  file_names = command_line_parser.GetFileNames()
-  
-  data = data_wrapper.DataFrameWrapper(file_names)
-    
-  if arguments.OPZ:
-    opz_generator.OPZGenerator(data)
-  if arguments.PDV:
-    pdv_generator.PDVGenerator(data)
-  if arguments.PDVS:
-    pdvs_generator.PDVSGenerator(data)
-  if arguments.ZP:
-    zp_generator.ZPGenerator(data)
+    command_line_parser = parser.Parser()
+    arguments = command_line_parser.GetArguments()
+    file_names = command_line_parser.GetFileNames()
+
+    data = data_wrapper.DataFrameWrapper(file_names)
+
+    if arguments.OPZ:
+        opz_generator.OPZGenerator(data)
+    if arguments.PDV:
+        pdv_generator.PDVGenerator(data)
+    if arguments.PDVS:
+        pdvs_generator.PDVSGenerator(data)
+    if arguments.ZP:
+        zp_generator.ZPGenerator(data)
 
 
-
-#Entry point for the runnable script (defined in setup.py)
+# Entry point for the runnable script (defined in setup.py)
 def Run():
-  sys.exit(Main())
+    sys.exit(Main())
 
-if __name__ == '__main__':
-  Run()
+
+if __name__ == "__main__":
+    Run()
